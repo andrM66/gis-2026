2484a01b4e4374eeb854340619cc0a16cc265843
Andrei <marinin1337@gmail.com>
2026-05-09 19:29:58 +0400

    lr3

--- client/vite-project/src/main.ts
+++ client/vite-project/src/main.ts
@@ -1,34 +1,47 @@
-import './style.css';
+import './style.css'
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile'; // Используем TileLayer для WMS
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
 import OSM from 'ol/source/OSM';
-import TileWMS from 'ol/source/TileWMS'; // Используем TileWMS
+import GeoJSON from 'ol/format/GeoJSON';
 import { fromLonLat } from 'ol/proj';
+import { stylefunction } from 'ol-mapbox-style';
+import TileWMS from 'ol/source/TileWMS';
 
-const map = new Map({
+const overtureSource = new VectorSource({
+  url: 'output.json',
+  format: new GeoJSON(),
+});
+
+const overtureLayer = new VectorLayer({ source: overtureSource });
+
+fetch('style.json')
+  .then((r) => r.json())
+  .then((style) => stylefunction(overtureLayer, style, 'overture'));
+
+new Map({
   target: 'map',
   layers: [
-    new TileLayer({
-      source: new OSM()
-    }),
-    // Рекомендуется объединять слои одного сервера в один запрос для производительности
-    new TileLayer({
+    new TileLayer({ source: new OSM() }),
+     new TileLayer({
       source: new TileWMS({
         url: 'http://localhost:8080/geoserver/gis/wms',
         params: {
-          // Убрали лишние пробелы и объединили слои через запятую
           'LAYERS': 'gis:buildings,gis:roads,gis:poi',
           'TILED': true
         },
         serverType: 'geoserver',
         transition: 0
       })
-    })
+    }),
+    overtureLayer,
   ],
   view: new View({
     center: fromLonLat([49.2826899, 53.6001943]),
-    zoom: 14
-  })
+    zoom: 14,
+  }),
 });
+
